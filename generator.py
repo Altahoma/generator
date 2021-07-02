fd6ed63211ee0cf9c4328bc825642d17a7dfccc2
altahoma <altahoma@gmail.com>
2021-07-02 17:59:24 +0300

    implemented generator

--- generator.py
+++ generator.py
@@ -1,1 +1,28 @@
-print('Hello Py!')
+import yaml
+from pprint import pprint
+
+
+with open('schema.yml') as f:
+    templates = yaml.safe_load(f)
+
+# pprint(templates)
+
+tables_list = list(templates)
+# print(tables_list)
+# print(list(templates.get(tables_list[0])))
+
+for table_name in templates:
+    # print(table_name)
+    table_fields = templates.get(table_name).get('fields')
+    # print(table_fields)
+    # print(templates.get(table_name))
+    # print(table_name)
+    print('CREATE TABLE', table_name.lower(), '(')
+    print(f'    {table_name.lower()}_id SERIAL PRIMARY KEY,')
+    fields = len(table_fields)
+    for field, value in table_fields.items():
+        print(f'    {table_name.lower()}_{field} {value} NOT NULL', end='')
+        if fields > 1:
+            print(',')
+            fields -= 1
+    print('\n);')
