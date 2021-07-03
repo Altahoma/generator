4209aad018615281b384239894d9d89932ecae64
altahoma <altahoma@gmail.com>
2021-07-03 21:32:26 +0300

    completed

--- generator.py
+++ generator.py
@@ -1,32 +1,57 @@
 import yaml
 
 
-def create_table_sql(schema):
+def create_table_sql(table_name, templates):
+    table = (
+        'CREATE TABLE {table_name} (\n'
+        '    {table_name}_id SERIAL PRIMARY KEY,\n'
+        '    {table_name}_created timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,\n'
+        '    {table_name}_updated timestamp,\n'
+    ).format(table_name=table_name.lower())
     table_fields = templates.get(table_name).get('fields')
-    print(f'CREATE TABLE {table_name.lower()} (')
-    print(f'    {table_name.lower()}_id SERIAL PRIMARY KEY,')
-    print(f'    {table_name.lower()}_created timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,')
-    print(f'    {table_name.lower()}_updated timestamp,')
     fields = len(table_fields)
     for field, value in table_fields.items():
-        print(f'    {table_name.lower()}_{field} {value} NOT NULL', end='')
+        table += '    {table_name}_{field} {value} NOT NULL'.format(
+            table_name=table_name.lower(),
+            field=field,
+            value=value
+        )
         if fields > 1:
-            print(',')
+            table += ',\n'
             fields -= 1
-    print('\n);')
+    table += '\n);'
+    return table
 
 
-def create_function_sql(schema):
-    pass
+def create_function_sql(table_name):
+    function = (
+        'CREATE OR REPLACE FUNCTION update_{table_name}_updated() RETURNS trigger AS $$\n'
+        '    BEGIN\n'
+        '        new.{table_name}_updated = CURRENT_TIMESTAMP;\n'
+        '    BEGIN\n'
+        '$$ LANGUAGE plpgsql;'
+    ).format(table_name=table_name.lower())
+    return function
 
 
-def create_trigger_sql(schema):
-    pass
+def create_trigger_sql(table_name):
+    trigger = (
+        'CREATE TRIGGER tr_{table_name}_updated BEFORE UPDATE ON article\n'
+        '    FOR EACH ROW EXECUTE FUNCTION update_{table_name}_updated();'
+    ).format(table_name=table_name.lower())
+    return trigger
 
 
-# add UI as 'Please type the file name in format ".yml" (For example: "schema.yml")
-with open('schema.yml') as f:
-    templates = yaml.safe_load(f)
+def parser():
+    print('Please enter the file name in the format ".yml" (For example: "schema.yml")')
+    schema = input()
+    with open(schema) as file:
+        templates = yaml.safe_load(file)
 
-for table_name in templates:
-    create_table_sql(table_name)
+        for table in templates:
+            print(create_table_sql(table, templates))
+            print(create_function_sql(table))
+            print(create_trigger_sql(table))
+
+
+parser()
